ac774d69be261ae758deaaba1e149a1ce952d04d
Patrick Smith <psmith5644@gmail.com>
2024-10-09 17:14:48 -0700

    wrtie testStateOffWhenSwitchOpen to fail

--- test/support/DeviceState/test_DeviceState.c
+++ test/support/DeviceState/test_DeviceState.c
@@ -12,3 +12,12 @@ void teardown(void) {
 void testInitStateOff(void) {
     TEST_ASSERT_EQUAL_UINT8(DeviceState_Off, DeviceState_GetState());
 }
+
+void testStateOffWhenSwitchOpen(void) {
+    PwrSwitch_Init();
+    FakePwrSwitch_Open();
+
+    TEST_ASSERT_EQUAL_UINT8(DeviceState_Off, DeviceState_GetState());
+
+    PwrSwitch_Destroy();
+}
