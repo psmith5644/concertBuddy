c499b10f6c04155878a39543067889aa1b224dcd
Patrick Smith <psmith5644@gmail.com>
2024-10-10 16:01:58 -0700

    start test-driving microphone: write testSamplePutInBuffer to fail

---
+++ test/Microphone/test_microphone.c
@@ -0,0 +1,21 @@
+#include "unity.h"
+
+static micSample * sampleData;
+
+void setup(void) {
+
+}
+
+void teardown() {
+
+}
+
+void testSampleOutput(void) {
+    Mic_Init();
+
+    micSample sample = FakeMic_Sample(1);
+
+    TEST_ASSERT_EQUAL_UINT32(1, sample);
+
+    Mic_Destroy();
+}
