ee6ffa51546e11bd6e5a50257f4c83086afe3681
Patrick Smith <psmith5644@gmail.com>
2024-10-17 17:45:23 -0700

    write testTwoSamples to fail

--- test/Microphone/test_MicrophoneController.c
+++ test/Microphone/test_MicrophoneController.c
@@ -20,3 +20,17 @@ void testSingleSample(void) {
     FakeMicrophoneDriver_Destroy();
     MicrophoneController_Destroy();
 }   
+
+void testTwoSamples(void) {
+    MicrophoneController_Init();
+    FakeMicrophoneDriver_Init();
+
+    FakeMicrophoneDriver_SetNextSample(1);
+    TEST_ASSERT_EQUAL_UINT32(1, MicrophoneController_Sample());
+
+    FakeMicrophoneDriver_SetNextSample(12);
+    TEST_ASSERT_EQUAL_UINT32(12, MicrophoneController_Sample());
+
+    FakeMicrophoneDriver_Destroy();
+    MicrophoneController_Destroy();
+}
