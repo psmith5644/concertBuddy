fe067ed6059cd148021e74f33c23043f8f1d1105
Patrick Smith <psmith5644@gmail.com>
2024-10-24 15:19:52 -0700

    write testGetSampleMatchesPreviouslySetSample to fail

--- test/Microphone/test_fakeADC.c
+++ test/Microphone/test_fakeADC.c
@@ -3,7 +3,6 @@
 
 /* 
 Test List:
-- the next sample is 0 if a nextsample has not been set.
 - the getnextsample() function returns whatever was the previous value in setnextsample()
 - there is an initialize oneshot function that sets the status flag to ready for oneshot samples
 - an initialize continuous function sets the status flag to ready for continuous samples
@@ -23,6 +22,15 @@ void testNoSampleResultZero(void) {
     FakeADC_Init();
 
     TEST_ASSERT_EQUAL_UINT32(0, FakeADC_GetNextSample());
+
+    FakeADC_Destroy();
+}
+
+void testGetSampleMatchesPreviouslySetSample(void) {
+    FakeADC_Init();
+
+    FakeADC_SetNextSample(1234);
+    TEST_ASSERT_EQUAL_UINT32(1234, FakeADC_GetNextSample());
 
     FakeADC_Destroy();
 }
