0e9515de947f5724576e8d23a4b7704828439ab8
Patrick Smith <psmith5644@gmail.com>
2024-10-24 14:36:40 -0700

    refactor tests to use setup/teardown

--- test/Microphone/test_MicrophoneDriver.c
+++ test/Microphone/test_MicrophoneDriver.c
@@ -2,33 +2,25 @@
 #include "microphoneDriver.h"
 #include "fakeADC.h"
 
-void setup(void) {
+uint8_t static const microphonePort = 33;
 
+void setup(void) {
+    MicrophoneDriver_Init(microphonePort);
 }
 
 void teardown(void) {
-
+    MicrophoneDriver_Destroy(microphonePort);
 }
 
 // driver is responsible for congifuring and initializng ADC/DMA, resulting with data in some buffer in RAM.
 void testInitDriver(void) {
-    uint8_t microphonePort = 33;
-    MicrophoneDriver_Init(microphonePort);
-
     TEST_ASSERT_EQUAL(MIC_STATUS_RDY, MicrophoneDriver_GetStatus(microphonePort));
-
-    MicrophoneDriver_Destroy(microphonePort);
 }
 
 void testSingleSample(void) {
-    uint8_t microphonePort = 33;
-    MicrophoneDriver_Init(microphonePort);
-
     FakeADC_SetNextSample(1234);
     micSample_t sample = MicrophoneDriver_Sample(microphonePort);
     TEST_ASSERT_EQUAL_UINT32(1234, sample);
-
-    MicrophoneDriver_Destroy(microphonePort);
 }
 
 
