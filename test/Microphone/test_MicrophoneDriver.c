b8c6fad157e899998a20a6d5ef62586ad379c63b
Patrick Smith <psmith5644@gmail.com>
2024-10-24 14:15:05 -0700

    change first test to reflect better design, where MicrophoneDriver has its own status for the user to interact with

--- test/Microphone/test_MicrophoneDriver.c
+++ test/Microphone/test_MicrophoneDriver.c
@@ -11,14 +11,13 @@ void teardown(void) {
 }
 
 // driver is responsible for congifuring and initializng ADC/DMA, resulting with data in some buffer in RAM.
-// therfore relies on fake ADC
-void testInitADC(void) {
+void testInitDriver(void) {
     uint8_t microphonePort = 33;
     MicrophoneDriver_Init(microphonePort);
 
-    TEST_ASSERT_EQUAL(ADC_STATUS_RDY, FakeADC_GetStatus(microphonePort));
+    TEST_ASSERT_EQUAL(MIC_STATUS_RDY, MicrophoneDriver_GetStatus(microphonePort));
 
-    MicrophoneDriver_Destroy();
+    MicrophoneDriver_Destroy(microphonePort);
 }
 
 
