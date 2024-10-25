e0ca52b0cd419d721e8ba3ae1f71a9157bb25e4a
Patrick Smith <psmith5644@gmail.com>
2024-10-25 12:18:42 -0700

    refactor testContinuousSamplingBufferResult

--- test/Microphone/test_MicrophoneDriver.c
+++ test/Microphone/test_MicrophoneDriver.c
@@ -42,7 +42,7 @@ void testContinuousSamplingBufferResult(void) {
     micSample_t buf[] = {0x12, 0x34, 0x56, 0x78};
     FakeADC_SetBuffer(buf);
 
-    micSample_t * dest;
+    micSample_t dest[] = {0x00, 0x00, 0x00, 0x00};
     MicrophoneDriver_ContinuousSamplingBegin(dest);
     MicrophoneDriver_ContinuousSamplingStop();
     TEST_ASSERT_EQUAL_UINT32_ARRAY(buf, dest, 4);
