ad40f23536fb75205fe30f5e5babb0c7b3721d08
Patrick Smith <psmith5644@gmail.com>
2024-10-25 12:24:40 -0700

    impl MicrophoneDriver_ContinuousSamplingBegin to passtestContinuousSamplingBufferResult

--- include/microphoneDriver.h
+++ include/microphoneDriver.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include "fakeADC.h"
 
 typedef enum {
     MIC_STATUS_RDY
@@ -12,3 +13,5 @@ void MicrophoneDriver_Init(uint8_t port);
 void MicrophoneDriver_Destroy(uint8_t port);
 MIC_STATUS MicrophoneDriver_GetStatus(uint8_t port);
 micSample_t MicrophoneDriver_Sample(uint8_t port);
+void MicrophoneDriver_ContinuousSamplingBegin(micSample_t * dest);
+void MicrophoneDriver_ContinuousSamplingStop();
--- src/microphoneDriver.c
+++ src/microphoneDriver.c
@@ -10,3 +10,9 @@ MIC_STATUS MicrophoneDriver_GetStatus(uint8_t port) {
 micSample_t MicrophoneDriver_Sample(uint8_t port) {
     return ADC_Sample();
 }
+
+void MicrophoneDriver_ContinuousSamplingBegin(micSample_t * dest) {
+    ADC_ContinuousSamplingBegin(dest);
+}
+
+void MicrophoneDriver_ContinuousSamplingStop() {}
