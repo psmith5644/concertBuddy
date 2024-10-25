fd14895b6490df550dbcacaa528f0075e6a26fbe
Patrick Smith <psmith5644@gmail.com>
2024-10-25 15:14:27 -0700

    pass testMultipleContinuousSamplingBufferResults

--- src/microphoneDriver.c
+++ src/microphoneDriver.c
@@ -1,7 +1,11 @@
 #include "microphoneDriver.h"
 
-void MicrophoneDriver_Init(uint8_t port) {}
-void MicrophoneDriver_Destroy(uint8_t port) {}
+void MicrophoneDriver_Init(uint8_t port) {
+    ADC_Init();
+}
+void MicrophoneDriver_Destroy(uint8_t port) {
+    ADC_Destroy();
+}
 
 MIC_STATUS MicrophoneDriver_GetStatus(uint8_t port) {
     return MIC_STATUS_RDY;
